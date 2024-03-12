47b8f38d23c8b3daf666c02baef70cb9b0a970b1
rajeev <rajeev.kumar.phe21@itbhu.ac.in>
2024-03-13 00:55:28 +0530

    structure added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+     welcome to app
+    </>
+
+  )
+}
+
+export default App
---
+++ src/Pages/rajeev.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const rajeev = () => {
+  return (
+    <div>
+      Hi Rajeev
+    </div>
+  )
+}
+
+export default rajeev
