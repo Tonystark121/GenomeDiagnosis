4987e0239f8bff3ada81feab682e1286aedbfa2a
rajeev <rajeev.kumar.phe21@itbhu.ac.in>
2024-04-04 11:56:41 +0530

    added dropdown

---
+++ src/Components/menu.js
@@ -0,0 +1,41 @@
+export const menuData = [
+  {
+    title:'Home'
+  },
+  {
+    title:'About Us'
+  },
+  {
+    title:'Testing Portfolio'
+  },
+  {
+    title:'Partners',
+    subTitle:[
+      {
+        title:'Sunpharma',
+      },{
+        title:'Mankind'
+      }
+    ]
+  },
+  {
+    title:'Patients'
+  },
+  {
+    title:'Careers'
+  },
+  {
+    title:'Events',
+    subTitle:[
+      {
+        title:'Scientific Community'
+      },
+      {
+        title:'News & Blogs'
+      },
+      {
+        title:'Events'
+      }
+    ]
+  }
+]
--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,9 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./nav.module.css";
 import { FaAngleDown } from "react-icons/fa6";
+import { menuData } from "./menu";
 
 const nav = () => {
-  const [showDropDown, setShowDropDown] = useState(true);
+  const [showDropDown, setShowDropDown] = useState(null);
+  const openLinks = (idx) => {
+    setShowDropDown(idx)
+  }
+  const closeLinks = (idx) => {
+    setShowDropDown(idx)
+    window.addEventListener('onClick',()=>{
+      setShowDropDown(null)
+    })
+  }
+
+  useEffect(()=>{
+    const handleClickOutside = (event) => {
+      if (!event.target.closest('.event_dropdown')) {
+        setShowDropDown(null);
+        window.removeEventListener('click', handleClickOutside);
+      }
+    }
+    window.addEventListener('click',handleClickOutside)
+  },[showDropDown])
+  const renderSubtitle = (subtitle) => {
+    return (
+      <ul className={styles.event_dropdown} >
+        {subtitle.map((ele, idx) => (
+          <li key={idx}>
+            {ele.title}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <>
       <nav className={styles.nav}>
@@ -16,41 +48,15 @@ const nav = () => {
             </span>
           </div>
           <ul className={styles.navlinks}>
-            <li>
-              Home <FaAngleDown className={styles.icon} />
-            </li>
-            <li>
-              About Us <FaAngleDown className={styles.icon} />
-            </li>
-            <li>
-              Testing Portfolio <FaAngleDown className={styles.icon} />
-            </li>
-            <li>
-              Patients <FaAngleDown className={styles.icon} />
-            </li>
-            <li>
-              Partners <FaAngleDown className={styles.icon} />
-            </li>
-            <li>
-              Careers <FaAngleDown className={styles.icon} />
-            </li>
-            <li>
-              Events <FaAngleDown className={styles.icon} />
-              {showDropDown && (
-                <div className={styles.event_dropdown}>
-                  <ul>
-                    <li>Scientific Community</li>
-                    <li>News and Events</li>
-                    <li>Blogs</li>
-                  </ul>
-                </div>
-              )}
-            </li>
+            {menuData.map((items, idx) => (
+              <li key={idx} id={`link${idx}`} onMouseEnter={()=>openLinks(idx)} onMouseLeave={()=>closeLinks(idx)} >
+                {items.title} <FaAngleDown className={styles.icon} />
+                {showDropDown===idx &&items.subTitle && renderSubtitle(items.subTitle)}
+              </li>
+            ))}
           </ul>
         </div>
-        <button>
-            Register
-        </button>
+        <button id="btn">Register</button>
       </nav>
     </>
   );
