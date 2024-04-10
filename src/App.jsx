88b88e6957afc8a23880c1cae544b3c02bba2749
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-04-10 23:19:09 +0530

    swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/nav'
 import CareerSections from './Components/home/CareerSections'
+import Swiper from './Components/home/Swiper'
 
 function App() {
 
   return (
     <>
        <Navbar />
+       <Swiper />
        <CareerSections/>
     </>
 
